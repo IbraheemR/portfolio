4d7ca60041b2bcceafa61af88e83cf0e4dbe85c5
Ibraheem <28712361+IbraheemR@users.noreply.github.com>
2023-07-01 22:26:29 +0100

    Add AW and reword some things

--- data/sites.ts
+++ data/sites.ts
@@ -1,5 +1,6 @@
 import rfc from "./images/rfc4.png";
 import oxcompsoc from "./images/oxcompsoc.png";
+import arabicworkshop from "./images/arabicworkshop.png";
 import mnist from "./images/mnist.png";
 import pakit from "./images/pakit.png";
 import catchment from "./images/catchment.png";
@@ -9,50 +10,65 @@ import story from "./images/story.png";
 
 export default [
     {
+        "name": "Arabic Workshop",
+        "description": "I'm currently working with Arabic Workshop to build a new mobile app to host their content library.",
+        "year": "2022 - ongoing",
+        "image": arabicworkshop,
+        "link": "https://ox.compsoc.net"
+    },
+    {
         "name": "Oxford Computing Society",
-        "description": "I handle server admin & web dev for the University of Oxford Computing Society.",
+        "description": "OxCompSoc approached me to build a new website to accept memberships and publicise events in time for their fresher's fair.",
+        "year": "2022 - 23",
         "image": oxcompsoc,
         "link": "https://ox.compsoc.net"
     },
     {
-        "name": "MNIST Explorer",
-        "description": "I lead a project with some friends to make an interactive demo for the classic MNIST neural network project.",
-        "image": mnist,
-        "link": "https://mnist.ibraheemrodrigues.com"
-    },
-    {
         "name": "Catchment Tool",
-        "description": "I developed this small web app back in 2019 for admin staff at a GP surgery.",
+        "description": "I developed this web app to make admin simpler for NHS staff.",
+        "year": "2019",
         "image": catchment,
         "link": "https://catchment.ibraheemrodrigues.com/?s=clapham"
     },
     {
+        "name": "MNIST Explorer",
+        "description": "I led this group project to create a realtime visulisation for the MNIST classifier AI.",
+        "year": "2021",
+        "image": mnist,
+        "link": "https://mnist.ibraheemrodrigues.com"
+    },
+    {
         "name": "PAKit",
-        "description": "My tool for working with Starbound's .pak modding files has recieved over 7000 downloads to date.",
+        "description": "My modding tool for working with Starbound's .pak files has recieved over 7000 downloads to date.",
+        "year": "2018",
         "image": pakit,
         "link": "https://pakit.ibraheemrodrigues.com"
     },
     {
         "name": "Robo Fight Club",
         "description": "My degree is super boring. So we created this secret tournament to have some fun and explore cool technology!",
+        "year": "2022",
         "image": rfc,
         "link": "https://robofc.netlify.app/"
     },
     {
         "name": "Oneshot",
-        "description": "I created this multiplayer game for the 2018 repl.it game jam. It works on mobile too!",
+        "description": "I created this multiplayer game for the repl.it game jam. It works on mobile too!",
+        "year": "2018",
         "image": oneshot,
         "link": "https://oneshot.ibraheemrodrigues.repl.co/"
     },
     {
         "name": "Minecraft Banner Maker",
         "description": "I made this web app because existing ones were either not up to date or super clunky.",
+        "year": "2020",
         "image": banner,
         "link": "https://banner.ibraheemrodrigues.com"
     },
     {
         "name": "Story Typer",
         "description": "A small experiment; make touch typing more fun by reading a story at the same time.",
+        "year": "2020",
         "image": story,
         "link": "https://type.ibraheemrodrigues.com",
         "centered": true
